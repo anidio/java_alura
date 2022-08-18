99c9da8f0a1a7931555f0eb4fecf61d8f1e4e6b4
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2022-08-18 10:01:18 -0300

    Bytebank-herdado
    polimorfismo e herança

---
+++ bytebank-herdado/src/TestaFuncionario.java
@@ -0,0 +1,18 @@
+
+public class TestaFuncionario {
+	
+	public static void main(String[] args) {
+		
+		Funcionario isaias = new Funcionario();
+		isaias.setNome("isaias roberto");
+		isaias.setCpf("22288855512");
+		isaias.setSalario(1274.00);
+		
+		System.out.println(isaias.getNome());
+		System.out.println(isaias.getBonificacao());
+		
+		System.out.println(isaias.getBonificacao());
+		
+	}
+
+}
